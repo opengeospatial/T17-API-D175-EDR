e6120e06241d87bdaf19fa40748a92ab18a4ab99
Bruno Lorente Cantarero <blorente@novadevs.com>
2021-10-27 06:44:57 +0200

    Add default zoom setup.

--- app.py
+++ app.py
@@ -18,6 +18,7 @@ DEFAULT_BBOX = "" # I.e. "-0.489,51.28,0.236,51.686" # LONDON
 DEFAULT_ELEMENT_ID = "" # I.e.  "CWFID.AEROFACP_1M.5212.5610.7AEB8DB2246327DC1F20020000" # HEATHROW
 TILESERVER_URL = 'https://tile.openstreetmap.org/{z}/{x}/{y}.png'
 GROUPS = get_groups(API_BASE_URL)
+DEFAULT_ZOOM = 12
 
 @app.route('/')
 def index():
@@ -34,8 +35,8 @@ def index():
         tileserver=TILESERVER_URL,
         collections = COLLECTIONS_IN_API,
         # queryables = QUERYABLES_IN_API,
-        groups = GROUPS
-
+        groups = GROUPS,
+        zoom=DEFAULT_ZOOM
     )
 
 @app.route('/collections/<collectionId>/items/', defaults={
